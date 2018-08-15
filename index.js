08fb4419650bf48f382561902ea49ea9adde8f46
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-15 16:41:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Driver {
    constructor(name, startDate) {
      this.name = name
-     this.startDate = newDate(startDate);
+     this.startDate = new Date(startDate);
    }
 
    yearsExperienceFromBeginningOf(year){
