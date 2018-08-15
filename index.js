8d01f754f2028291bb947da5bfac1692ed24fc5d
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-15 16:40:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
 class Driver {
-  constructor(name, startDate){
-    this.name = name
-    this.startDate = new Date(startDate)
-  }
+   constructor(name, startDate) {
+     this.name = name
+     this.startDate = new Date(startDate);
+   }
+
+   yearsExperienceFromBeginningOf(year){
+     return year - this.startDate.getFullYear()
+   }
 }
 
-Driver.prototype.yearsExperienceFromBeginningOf = function(year){
-  return year - this.startDate.getFullYear();
-}
+let eastWest= ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue', '6th Avenue', '7th Avenue','8th Avenue','9th Avenue','10th Avenue']
 
 class Route {
-  constructor(beginningLocation, endingLocation){
-    this.beginningLocation = beginningLocation
-    this.endingLocation = endingLocation
+  constructor(beginningLocation,endingLocation){
+      this.beginningLocation = beginningLocation
+      this.endingLocation = endingLocation;
   }
-}
 
-Route.prototype.blocksTravelled = function(beginningLocation, endingLocation){
-  let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-
-  function distanceAsIndex(avenue){
+  avenueToInteger(avenue){
     return eastWest.indexOf(avenue)
   }
 
-  const horizontalDistance = distanceAsIndex(this.beginningLocation.horizontal) - distanceAsIndex(this.endingLocation.horizontal)
-  const verticalDistance = this.beginningLocation.vertical - this.endingLocation.vertical
-
-  return Math.abs(totalDistance = horizontalDistance + verticalDistance);
-}
-
-Route.prototype.estimatedTime = function(peak){
-  if (peak) {
-    return this.blocksTravelled() / 2
+  blocksTravelled() {
+    let horizontalChange = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
+    let verticalChange = this.endingLocation.vertical - this.beginningLocation.vertical
+    return (Math.abs(horizontalChange)) + (Math.abs(verticalChange))
   }
-  else {
-    return this.blocksTravelled() / 3
+
+  estimatedTime(peak){
+    if(peak) {
+      return this.blocksTravelled()/2
+    }
+    else {
+      return this.blocksTravelled()/3
+    }
   }
 }
